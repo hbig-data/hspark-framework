a006dc0eb2fadab5741922b4831a67a73d36d2f0
liuhuan <liuwei41255703@163.com>
2016-07-08 09:46:59 +0800

    Spark 读取 HBase中的数据。

--- src/main/java/com/hspark/job/core/SparkReadHBase.java
+++ src/main/java/com/hspark/job/core/SparkReadHBase.java
@@ -13,6 +13,8 @@ import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
+import scala.Tuple2;
 
 import java.io.Serializable;
 
@@ -24,25 +26,40 @@ import java.io.Serializable;
  */
 public class SparkReadHBase implements Serializable {
 
+    private static final String COLUMN_FAMLILY = "info";
+    private static final String COLUMN_NAME = "time";
+
 
     public static void main(String[] args) {
-        SparkConf config = new SparkConf().setAppName("ReadHBase").setMaster("local[1]").set("spark.executor.memory", "64m");
+        SparkConf config = new SparkConf().setAppName("ReadHBase").setMaster("local[1]").set("spark.executor.memory", "500m");
         JavaSparkContext jsc = new JavaSparkContext(config);
 
         Configuration conf = HBaseConfiguration.create();
         Scan scan = new Scan();
-        scan.addFamily(Bytes.toBytes("cf"));
-        scan.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("airName"));
+        scan.addFamily(Bytes.toBytes(COLUMN_FAMLILY));
+        scan.addColumn(Bytes.toBytes(COLUMN_FAMLILY), Bytes.toBytes(COLUMN_NAME));
 
         try {
-            String tableName = "flight_wap_order_log";
+            String tableName = "scantest";
             conf.set(TableInputFormat.INPUT_TABLE, tableName);
             ClientProtos.Scan proto = ProtobufUtil.toScan(scan);
+
             String ScanToString = Base64.encodeBytes(proto.toByteArray());
             conf.set(TableInputFormat.SCAN, ScanToString);
 
             JavaPairRDD<ImmutableBytesWritable, Result> myRDD = jsc.newAPIHadoopRDD(conf, TableInputFormat.class, ImmutableBytesWritable.class, Result.class);
 
+            myRDD.map(new Function<Tuple2<ImmutableBytesWritable,Result>, Tuple2>() {
+                @Override
+                public Tuple2 call(Tuple2<ImmutableBytesWritable, Result> v1) throws Exception {
+
+                    byte[] o = v1._2().getValue(Bytes.toBytes(COLUMN_FAMLILY), Bytes.toBytes(COLUMN_NAME));
+                    if (o != null) {
+                        return new Tuple2<Integer, Integer>(Bytes.toInt(o), 1);
+                    }
+                    return null;
+                }
+            });
 
             System.out.println(myRDD.count());
 
---
+++ src/main/java/com/hspark/job/streaming/HelloSparkKafka.java
@@ -0,0 +1,118 @@
+package com.hspark.job.streaming;
+
+import kafka.serializer.StringDecoder;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.streaming.Duration;
+import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaPairInputDStream;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import org.apache.spark.streaming.kafka.HasOffsetRanges;
+import org.apache.spark.streaming.kafka.KafkaUtils;
+import org.apache.spark.streaming.kafka.OffsetRange;
+import scala.Tuple2;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicReference;
+
+/**
+ * @author Rayn
+ * @email liuwei412552703@163.com
+ * Created by Rayn on 2016/7/7 17:43.
+ */
+public class HelloSparkKafka implements Serializable {
+
+    /**
+     * 写offset 到 zookeeper
+     *
+     * @param zkClient
+     * @param zkPathRoot
+     * @param offsetRanges
+     */
+    public static void writeOffsetToZookeeper(String zkClient, String zkPathRoot, OffsetRange[] offsetRanges) {
+
+    }
+
+    /**
+     * @param rdd
+     * @param outputFolderPath
+     * @param definedDuration
+     */
+    public static long processEachRDD(JavaRDD<String> rdd, String outputFolderPath, String definedDuration) {
+
+        return 0;
+    }
+
+
+    public static void main(String[] args) {
+
+        final String zkClient = "";
+        final String zkPathRoot = "/kafka/offset";
+        final String outputFolderPath = "E:/test";
+        final String definedDuration = "E:/test";
+
+        Map<String, String> kafkaParams = new HashMap<String, String>();
+        Set<String> topicsSet = new HashSet<String>();
+
+
+        JavaSparkContext jssc = new JavaSparkContext("local[1]", "kafkaSpark", "", new String[]{});
+        JavaStreamingContext jsc = new JavaStreamingContext(jssc, Duration.apply(3 * 1_000));
+
+        JavaPairInputDStream<String, String> messages = KafkaUtils.createDirectStream(jsc, String.class, String.class, StringDecoder.class, StringDecoder.class, kafkaParams, topicsSet);
+
+        messages.foreachRDD(new Function<JavaPairRDD<String, String>, Void>() {
+            @Override
+            public Void call(JavaPairRDD<String, String> rdd) throws Exception {
+                OffsetRange[] offsets = ((HasOffsetRanges) rdd.rdd()).offsetRanges();
+                JavaRDD<String> valueRDD = rdd.values();
+                long msgNum = processEachRDD(valueRDD, outputFolderPath, definedDuration);
+                if (msgNum > 0 && zkPathRoot != null) {
+                    writeOffsetToZookeeper(zkClient, zkPathRoot, offsets);
+                }
+                return null;
+            }
+        });
+
+
+        /**
+         * =========================================================================================================
+         */
+        JavaPairInputDStream<String, String> messages2 = KafkaUtils.createDirectStream(jsc, String.class, String.class, kafka.serializer.StringDecoder.class, kafka.serializer.StringDecoder.class, kafkaParams, topicsSet);
+
+        final AtomicReference<OffsetRange[]> offsetRanges = new AtomicReference();
+        JavaDStream<String> lines2 = messages2.transformToPair(new Function<JavaPairRDD<String, String>, JavaPairRDD<String, String>>() {
+
+
+            @Override
+            public JavaPairRDD<String, String> call(JavaPairRDD<String, String> rdd) throws Exception {
+                OffsetRange[] offsets = ((HasOffsetRanges) rdd.rdd()).offsetRanges();
+                offsetRanges.set(offsets);
+                return rdd;
+            }
+        }).map(new Function<Tuple2<String, String>, String>() {
+            @Override
+            public String call(Tuple2<String, String> tuple2) {
+                return tuple2._2();
+            }
+        });
+
+        lines2.foreachRDD(new Function<JavaRDD<String>, Void>() {
+            @Override
+            public Void call(JavaRDD<String> rdd) throws Exception {
+                long msgNum = processEachRDD(rdd, outputFolderPath, definedDuration);
+                if (msgNum > 0 && zkPathRoot != null) {
+                    OffsetRange[] offsets = offsetRanges.get();
+                    writeOffsetToZookeeper(zkClient, zkPathRoot, offsets);
+
+                }
+                return null;
+            }
+        });
+    }
+}
