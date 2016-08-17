fb5cbd726ae10cdfc785135bf956b7c19b3d9311
liuhuan <liuwei41255703@163.com>
2016-08-17 11:01:06 +0800

    没啥更新的。

--- src/main/java/com/hspark/job/core/HelloSparkJob.java
+++ src/main/java/com/hspark/job/core/HelloSparkJob.java
@@ -1,9 +1,13 @@
 package com.hspark.job.core;
 
+import org.apache.spark.Accumulable;
+import org.apache.spark.Accumulator;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.VoidFunction;
+import org.apache.spark.storage.StorageLevel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -37,15 +41,34 @@ public class HelloSparkJob implements Callable<String>, Serializable {
 
 
         JavaSparkContext context = new JavaSparkContext(conf);
+
+        final Accumulator<Integer> accumulator = context.accumulator(0);
+
         JavaRDD<Integer> map = context.parallelize(data).map(new Function<String, Integer>() {
             @Override
             public Integer call(String s) throws Exception {
                 String[] split = s.split("-");
+
+                accumulator.add(1);
+
                 return Integer.valueOf(split[1]);
             }
         });
 
+        log.info("当前 Count : {}", accumulator.localValue());
 
+
+        JavaRDD<Integer> persist = map.persist(StorageLevel.DISK_ONLY());
+
+
+        persist.foreach(new VoidFunction<Integer>() {
+            @Override
+            public void call(Integer integer) throws Exception {
+                log.info("当前值:{}", integer);
+            }
+        });
+
+        log.info("缓存结束。");
 
         map.saveAsObjectFile("file:///E:/test/spark/result1");
 
