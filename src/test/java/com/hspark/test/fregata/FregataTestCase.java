285b966bd29dd3deabb9319cc4d871c5f2719eb7
liuhuan <liuwei41255703@163.com>
2016-12-21 11:02:34 +0800

    添加  fregata 库。

---
+++ src/test/java/com/hspark/test/fregata/FregataTestCase.java
@@ -0,0 +1,28 @@
+package com.hspark.test.fregata;
+
+import fregata.spark.data.LibSvmReader;
+import org.junit.After;
+import org.junit.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @Author Rayn
+ * @Vendor liuwei412552703@163.com
+ * Created by Rayn on 2016/12/21 10:45.
+ */
+public class FregataTestCase {
+    private static final Logger LOG = LoggerFactory.getLogger(FregataTestCase.class);
+
+
+    @Before
+    public void setUp() throws Exception {
+//        LibSvmReader.read(sc, trainPath, numFeatures.toInt);
+//        LibSvmReader.read(sc, testPath, numFeatures.toInt);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+}
