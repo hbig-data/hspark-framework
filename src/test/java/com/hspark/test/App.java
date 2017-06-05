79018f0cb54eea6735094b05834e0cf407cdbeb4
Ryan-瑞恩 <liuwei41255703@163.com>
2017-06-05 18:24:50 +0800

    删除最加写文件。

--- src/test/java/com/hspark/test/App.java
+++ src/test/java/com/hspark/test/App.java
@@ -1,26 +1,12 @@
 package com.hspark.test;
 
 import com.alibaba.fastjson.JSONObject;
-import kafka.metrics.KafkaCSVMetricsReporter;
-import kafka.metrics.KafkaMetricsConfig;
-import kafka.metrics.KafkaMetricsReporter;
-import kafka.metrics.KafkaMetricsReporterMBean;
-import kafka.server.KafkaConfig;
-import kafka.utils.Utils;
-import net.sf.json.JSON;
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import scala.collection.Iterator;
-import scala.collection.Seq;
-import scala.collection.mutable.StringBuilder;
 
 import java.io.File;
 import java.io.Serializable;
-import java.util.Properties;
 
 /**
  * @author Rayn on 2016/7/2.
@@ -48,7 +34,7 @@ public class App {
 }
 
 
-class Person implements Serializable{
+class Person implements Serializable {
     private String uname;
     private String upass;
 
